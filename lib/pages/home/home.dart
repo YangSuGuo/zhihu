0a0f561271383f6f9f84755fea3efabc2faae66a
YangSuGuo <3130876713@qq.com>
2024-11-08 10:27:26 +0800

    开始

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,5 +1,7 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
 import 'package:intl/intl.dart';
 import 'package:zhihu/pages/daily_report/daily_report.dart';
 import 'package:zhihu/pages/main.dart';
@@ -32,10 +34,107 @@ class Home extends StatelessWidget {
                         ),
                         padding: EdgeInsets.only(
                             bottom: 28.h, left: 24.w, right: 24.w),
-                          children: const [
-                            SizedBox(
-                              width: 1,
-                              child: Text("data"),
+                          children: [
+                            Container(
+                              height: 225,
+                              padding: const EdgeInsets.all(16),
+                              decoration: BoxDecoration(
+                                  color: Theme.of(context).colorScheme.primary,
+                                  borderRadius: BorderRadius.circular(35)),
+                              child: Column(
+                                children: [
+                                  Column(
+                                    children: [
+                                      Container(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width -
+                                              50,
+                                          height: 140,
+                                          child: ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(35),
+                                            child: AspectRatio(
+                                              aspectRatio: 1.5,
+                                              child: Image.asset(
+                                                "assets/images/png/macos_3840x2160.jpg",
+                                                fit: BoxFit.cover,
+                                              ),
+                                            ),
+                                          )),
+                                      Gap(5),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              IconButton(
+                                                onPressed: () {},
+                                                icon: Icon(Icons.ice_skating),
+                                                color: Theme.of(context)
+                                                    .colorScheme
+                                                    .primary,
+                                                style: IconButton.styleFrom(
+                                                  backgroundColor:
+                                                      Theme.of(context)
+                                                          .colorScheme
+                                                          .onPrimary,
+                                                ),
+                                              ),
+                                              Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  AutoSizeText(
+                                                    "我的订阅",
+                                                    style: TextStyle(
+                                                        fontSize: 17,
+                                                        color: Theme.of(context)
+                                                            .colorScheme
+                                                            .onPrimary,
+                                                        fontWeight:
+                                                            FontWeight.w500),
+                                                  ),
+                                                  AutoSizeText(
+                                                    "我的订阅我的订",
+                                                    style: TextStyle(
+                                                        fontSize: 13,
+                                                        color: Theme.of(context)
+                                                            .colorScheme
+                                                            .onPrimary
+                                                            .withAlpha(210),
+                                                        fontWeight:
+                                                            FontWeight.w500),
+                                                  ),
+                                                ],
+                                              )
+                                            ],
+                                          ),
+                                          SizedBox(
+                                              // width: 20,
+                                              // height: 20,
+                                              child: IconButton(
+                                            onPressed: () {},
+                                            icon: Icon(Icons.favorite_border),
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .primary,
+                                            style: IconButton.styleFrom(
+                                              // fixedSize: Size(30, 30),
+                                              backgroundColor: Theme.of(context)
+                                                  .colorScheme
+                                                  .onPrimary,
+                                            ),
+                                          ))
+                                        ],
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
                             )
                           ]));
                 },
